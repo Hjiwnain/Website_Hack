e02c9159f29a4c872e49f8817e8c3df58659b75a
Ayush <80757890+ayush645@users.noreply.github.com>
2023-02-01 12:03:23 +0530

    validation

--- public/script.js
+++ public/script.js
@@ -3,15 +3,152 @@ const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
-const nppm = document.getElementById("username").value;
 
 
 let formStepsNum = 0;
 
+function validate(formStepsNum){
+  var nppm = document.forms.RegForm.nppm.value;
+  var ls = document.forms.RegForm.ls.value;
+
+  var objective = document.forms.RegForm.objective.value;
+
+  var amount = document.forms.RegForm.amount.value;
+
+  var guarantor = document.forms.RegForm.guarantor.value;
+
+  var experience = document.forms.RegForm.experience.value;
+
+  var maritalStatus = document.forms.RegForm.maritalStatus.value;
+
+  var existingLoan = document.forms.RegForm.existingLoan.value;
+
+  var age = document.forms.RegForm.age.value;
+
+  var ca_balance = document.forms.RegForm.ca_balance.value;
+
+  var sa_balance = document.forms.RegForm.sa_balance.value;
+
+  var pi = document.forms.RegForm.pi.value;
+
+  var workab = document.forms.RegForm.workab.value;
+
+  var phn = document.forms.RegForm.phn.value;
+
+  var tenure = document.forms.RegForm.tenure.value;
+  var prop = document.forms.RegForm.prop.value;
+  var jobType = document.forms.RegForm.jobType.value;
+
+  var houset = document.forms.RegForm.houset.value;
+  var noe = document.forms.RegForm.noe.value;
+
+
+
+
+  if(nppm == ""){
+    window.alert("Please enter number of Maintainers.");
+                    name.focus();
+                    return false;
+  }
+  if(ls == "Choose"){
+    window.alert("Please select loan status.");
+                    name.focus();
+                    return false;
+  }
+  if(objective == "Choose"){
+    window.alert("Please select your objective.");
+                    name.focus();
+                    return false;
+  }
+  
+  
+  if(amount == ""){
+    window.alert("Please enter Amount.");
+                    name.focus();
+                    return false;
+  }
+
+//  if(formSteps == 1){
+//   if(guarantor == "Choose"){
+//     window.alert("Please enter your name properly.");
+//                     name.focus();
+//                     return false;
+//   }
+//   if(experience == "Choose"){
+//     window.alert("Please enter your name properly.");
+//                     name.focus();
+//                     return false;
+//   }
+//   if(maritalStatus == "Choose"){
+//     window.alert("Please enter your name properly.");
+//                     name.focus();
+//                     return false;
+//   }
+//   if(existingLoan == ""){
+//     window.alert("Please enter your name properly.");
+//                     name.focus();
+//                     return false;
+//   }
+// }
+  // if(ca_balance == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(sa_balance == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(pi == ""){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(workab == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(phn == ""){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(tenure == ""){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(prop == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(jobType == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+  // if(houset == "Choose"){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+
+  // if(noe == ""){
+  //   window.alert("Please enter your name properly.");
+  //                   name.focus();
+  //                   return false;
+  // }
+
+}
+
 nextBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     
-      
+    validate(formStepsNum);
+
       formStepsNum++;
       updateFormSteps();
       updateProgressbar();
